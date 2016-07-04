2a56349375be2228691f5ae706128f4557db7fab
michbur <michalburdukiewicz@gmail.com>
2016-07-04 22:03:49 +0200

    added read_fonts

--- read_pic.R
+++ read_pic.R
@@ -4,4 +4,17 @@
 
 library(EBImage)
 
-font_0 <- readImage("./fonts/png/0.png")
+# read fonts -------------------------------------------------------
+
+# for (single_file in list.files("./fonts/png/")) {
+#   img <- readImage(paste0("./fonts/png/", single_file))
+#   img_name <- paste0("font_", strsplit(single_file, ".", fixed = TRUE)[[1]][1])
+#   assign(img_name, img)
+# }
+
+fonts <- lapply(list.files("./fonts/png/"), function(single_file) {
+  img <- readImage(paste0("./fonts/png/", single_file))
+  slot(img, ".Data")[, , 1]
+})
+  
+names(fonts) <- paste0("f_", unlist(strsplit(list.files("./fonts/png/"), ".png", fixed = TRUE)))
